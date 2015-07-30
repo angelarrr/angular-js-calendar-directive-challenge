387b14715776b8650ff12b26885a9761218f7c45
angelarrr <kwon.angela@gmail.com>
2015-07-30 13:16:02 -0400

    begin adding functionality

--- app/app.js
+++ app/app.js
@@ -1,3 +1,20 @@
-angular.module('calendarDemoApp', []);
+angular.module('calendarDemoApp', [])
 
 // your controller and directive code go here
+	.directive('calendar', function(){
+		return {
+			restrict: 'E',
+			transclude: true,
+			templateUrl: 'calendar.html',
+			scope: true,
+			controller: function controller($scope, $element, $attrs){
+				var today = new Date();
+				var currMonth = today.getMonth();
+				var currYear = today.getFullYear();
+
+				$scope.month = currMonth;
+				$scope.year = currYear;
+				console.log($scope.month, $scope.year);
+			}
+		}
+	})
