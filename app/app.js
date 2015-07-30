86c04df84fa8e79aded9cbb873cdd2e0078d0698
angelarrr <kwon.angela@gmail.com>
2015-07-30 14:29:41 -0400

    update directive and controller

--- app/app.js
+++ app/app.js
@@ -1,6 +1,4 @@
 angular.module('calendarDemoApp', [])
-
-// your controller and directive code go here
 	.directive('calendar', function(){
 		return {
 			restrict: 'E',
@@ -12,9 +10,30 @@ angular.module('calendarDemoApp', [])
 				var currMonth = today.getMonth();
 				var currYear = today.getFullYear();
 
+				// select show current month and year
 				$scope.month = currMonth;
 				$scope.year = currYear;
-				console.log($scope.month, $scope.year);
+
+				// populate yr select box for 20yrs in past/future
+				var years = [];
+				for (var i = currYear - 20; i <= currYear + 20; i ++) {
+					years.push(i);
+				}
+				$scope.years = years;
+
+				// generate days that should be displayed for given month
+				$scope.getCal = function(year, month){
+					$scope.range = CalendarRange.getMonthlyRange(new Date(year, month));
+				};
+
+				// function to update calendar on select
+				$scope.updateCal = function(){
+					currMonth = $scope.month;
+					$scope.getCal($scope.year, $scope.month);
+				};
+
+				// load calendar
+				$scope.getCal(currYear, currMonth);
 			}
-		}
+		};
 	})
